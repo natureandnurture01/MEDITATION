64696dca23fdea2642fe6bdcb83188afe6aad7a5
natureandnurture01 <natureandnurture01@gmail.com>
2024-10-16 17:21:51 +0530

    Add files via upload

---
+++ run_visual_detection.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs the check_similarity.py script
+
+# Navigate to the directory where the Python script is located
+cd ~/Desktop/DADN/meditation_visualRohan
+
+# Run the Python script
+python3 check_similarity.py
